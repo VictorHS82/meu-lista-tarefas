1bf086f621dafda3bbb416ed5332acb0fa979d28
calouros <calouros@fatec.corp>
2026-02-25 21:42:05 -0300

    Mais coisas

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Meu App de Lista de Tarefas</Text>
+        </View>
+    )
+}
